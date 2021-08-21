927f9eb6be1cda10f77c5cd6e692129b03c8460c
Tony1-arch <67202712+Tony1-arch@users.noreply.github.com>
2021-08-21 23:02:20 +0400

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '91bd82c39b7dd2bb92c8240745ab08aa'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de73e28f-e5fa-4e95-a95d-b78eba901097'
+};
